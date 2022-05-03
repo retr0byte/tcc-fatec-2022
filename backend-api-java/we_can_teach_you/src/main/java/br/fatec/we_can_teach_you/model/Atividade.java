348c1430227933e0c7b8ad22eaa614c6be2441fc
retr0byte <dev.gbcproject@gmail.com>
2022-05-02 23:55:23 -0300

    back:java data layer - Activities #32

---
+++ backend-api-java/we_can_teach_you/src/main/java/br/fatec/we_can_teach_you/model/Atividade.java
@@ -0,0 +1,40 @@
+package br.fatec.we_can_teach_you.model;
+
+import java.util.Calendar;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name="Atividades")
+public class Atividade extends AbstractEntity{
+    private static final long serialVersionUID = 1L;
+
+    @Column(name="nm_Atividade")
+    private String titulo;
+
+    @Column(name="ds_Atividade")
+    private String descricao;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
+    private Calendar dataEntrega;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Evento> eventos;
+}
