4282652552f82bd1ce3cce4aa2af822e910446a9
Gustavo Arakaki <gustavo.arakaki@fatec.sp.gov>
2022-05-18 16:19:38 -0300

    front:ang faq page #60

--- frontend-angular/we_can_teach_you/src/app/app-routing.module.ts
+++ frontend-angular/we_can_teach_you/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FaqComponent } from './faq/faq.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'faq', component: FaqComponent}
+
 ];
 
 @NgModule({
