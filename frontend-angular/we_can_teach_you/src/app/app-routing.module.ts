86530b957abab1200bf8098c610faaa166b65d8d
luca2613 <luca.oliveira@fatec.sp.gov.br>
2022-05-18 21:06:46 -0300

    merge3 (resolved conflicts)

--- frontend-angular/we_can_teach_you/src/app/app-routing.module.ts
+++ frontend-angular/we_can_teach_you/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FaqComponent } from './faq/faq.component';
 import { HomeComponent } from './home/home.component';
 import { MinhasAulasComponent } from './minhas-aulas/minhas-aulas.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'faq', component: FaqComponent},
   { path: 'minhas-aulas', component: MinhasAulasComponent }
   
 ];
--- frontend-angular/we_can_teach_you/src/app/app.module.ts
+++ frontend-angular/we_can_teach_you/src/app/app.module.ts
@@ -5,16 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccessComponent } from './access/access.component';
 import { HomeComponent } from './home/home.component';
+<<<<<<< HEAD
+import { FaqComponent } from './faq/faq.component';
+=======
 import { MinhasAulasComponent } from './minhas-aulas/minhas-aulas.component';
+<<<<<<< HEAD
 import { MarcacoesComponent } from './marcacoes/marcacoes.component';
+=======
+>>>>>>> b468918defe928aefd936291ab824dcb3e5e48a0
+>>>>>>> 84d399584254f5bae58cb9a79c43ac6009709167
 
 @NgModule({
   declarations: [
     AppComponent,
     AccessComponent,
     HomeComponent,
+<<<<<<< HEAD
     MinhasAulasComponent,
     MarcacoesComponent
+=======
+<<<<<<< HEAD
+    FaqComponent
+=======
+    MinhasAulasComponent
+>>>>>>> b468918defe928aefd936291ab824dcb3e5e48a0
+>>>>>>> 84d399584254f5bae58cb9a79c43ac6009709167
   ],
   imports: [
     BrowserModule,
