c041d294948f2da12e025861da41f1e46c8e1b0d
luca2613 <luca.oliveira@fatec.sp.gov.br>
2022-06-08 19:46:37 -0300

    frontend:ang interface categorias API #94

--- frontend-angular/we_can_teach_you/src/app/model/Category.ts
+++ frontend-angular/we_can_teach_you/src/app/model/Category.ts
@@ -1,5 +1,12 @@
 export interface Category {
+	id?:number;
+	nome: string;
+	descricao: string;
+}
+
+export interface CategoryResponse {
 	id: number;
 	nome: string;
 	descricao: string;
 }
+
--- frontend-angular/we_can_teach_you/src/app/view/categorias/categorias.component.ts
+++ frontend-angular/we_can_teach_you/src/app/view/categorias/categorias.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertsService } from 'src/app/controller/alerts.service';
+import { ControllerService } from 'src/app/controller/controller.service';
+import { Category } from 'src/app/model/Category';
 
 @Component({
   selector: 'app-categorias',
@@ -8,20 +11,57 @@ import { Component, OnInit } from '@angular/core';
 export class CategoriasComponent implements OnInit {
 
   visivel:Boolean = false;
-  editVisivel:Boolean = false;
-  nome:string = 'Nome categoria';
-  descricao:string = 'Descrição categoria';
+  categories: Category | null = null;
+  categoryNome: string = '';
+  categoryDescricao: string = '';
+  
 
-  constructor() { }
+  constructor(public ctrl: ControllerService, public alerts: AlertsService) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.ctrl.getCategories();
+  }
 
   mostrar(): void {
     this.visivel = ! this.visivel; 
   }
 
-  editar() {
-    this.editVisivel = ! this.editVisivel;
+  editar(index: number) {
+    const el = document.getElementById(`cat-item-${index}`) as HTMLElement;
+    const editWrapper = el.querySelector(`.edit-wrapper`) as HTMLElement;
+    const staticWrapper = el.querySelector(`.static-wrapper`) as HTMLElement;
+
+    const btnEdit = el.querySelector(`.btn-edit`) as HTMLElement;
+    const btnSave = el.querySelector(`.btn-save`) as HTMLElement;
+
+    if(el.classList.contains("static")) {
+      editWrapper!.style.display = "flex";
+      staticWrapper!.style.display = "none";
+      btnEdit!.style.display = "none";
+      btnSave!.style.display = "flex";
+      el.classList.remove("static");
+
+    } else {
+      editWrapper!.style.display = "none";
+      staticWrapper!.style.display = "flex";
+      btnEdit!.style.display = "flex";
+      btnSave!.style.display = "none";
+      el.classList.add("static");
+
+    }
+    
+  }
+
+  cadCategory() {
+    this.ctrl.postCategory({
+      nome: this.categoryNome,
+      descricao: this.categoryDescricao
+    });
+
+    this.categoryNome = '';
+    this.categoryDescricao = '';
+    this.visivel = false;
+
   }
 
 }
